bb4593595f461843ed635f496972f9b54eaa6d21
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-09-19 22:41:26 +0300

    second

---
+++ bhggfc.js
@@ -0,0 +1,11 @@
+
+    const wrapper = document.createElement('div');
+    const name = document.createElement('div');
+    const surname = document.createElement('div');
+    const age = document.createElement('div');
+
+
+
+
+
+}
