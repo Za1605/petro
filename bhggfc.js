10aa8d6dc93e8e01fbec8df78935049f905433b1
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-09-19 22:41:26 +0300

    third

---
+++ bhggfc.js
@@ -0,0 +1,26 @@
+const form = document.forms.full;
+form.onsubmit = function (e){
+    e.preventDefault();
+
+    const wrapper = document.createElement('div');
+    const name = document.createElement('div');
+    const surname = document.createElement('div');
+    const age = document.createElement('div');
+
+    name.innerText = `Name:${this.name.value}`
+    surname.innerText = `Surname :${this.surname.value}`
+    age.innerText = `Age :${this.age.value}`
+
+    this.name.value = '';
+    this.surname.value = '';
+    this.age.value = '';
+
+    wrapper.append(name, surname,age);
+    document.body.appendChild(wrapper);
+
+
+
+    this.name.value = '';
+    this.surname.value = '';
+    this.age.value = '';
+}
